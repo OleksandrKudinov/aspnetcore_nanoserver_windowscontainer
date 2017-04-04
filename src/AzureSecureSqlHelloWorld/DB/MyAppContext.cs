9993194731750224efe7ad6b69ab830bde94c46f
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-04-04 18:11:25 +0300

    added EF DbContext;

---
+++ src/AzureSecureSqlHelloWorld/DB/MyAppContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureSecureSqlHelloWorld.DB
+{
+    public class MyAppContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public MyAppContext(DbContextOptions options)
+            : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("db_students");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
