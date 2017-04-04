c5f361ab8d4538215660091b6017f25dce1512f3
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-04-04 18:10:51 +0300

    add Student model

---
+++ src/AzureSecureSqlHelloWorld/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AzureSecureSqlHelloWorld.Models
+{
+    public class Student
+    {
+        [Key]
+        public Int32 Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+    }
+}
